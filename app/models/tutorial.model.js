6e6546ed98d5fd7436709a3847e744effb6ebb7d
Ahmad Faqih Arifin <ahmadfaqih796@gmail.com>
2023-01-23 23:00:24 +0700

    add phone model

--- app/models/tutorial.model.js
+++ app/models/tutorial.model.js
@@ -6,6 +6,9 @@ module.exports = (sequelize, Sequelize) => {
     description: {
       type: Sequelize.STRING,
     },
+    phone: {
+      type: Sequelize.INTEGER,
+    },
     published: {
       type: Sequelize.BOOLEAN,
     },
