ae29a998696c847c9d39f0455a258abdb33a3e55
Ahmad Faqih Arifin <ahmadfaqih796@gmail.com>
2023-01-25 09:39:05 +0700

    env

--- config/db.config.js
+++ config/db.config.js
@@ -1,8 +1,9 @@
+require('dotenv').config();
 module.exports = {
-  HOST: "localhost",
-  USER: "root",
-  PASSWORD: "",
-  DB: "sequalize",
+  HOST: process.env.HOST,
+  USER: process.env.USER,
+  PASSWORD: process.env.PASSWORD,
+  DB: process.env.DATABASE,
   dialect: "mysql",
   pool: {
     max: 5,
